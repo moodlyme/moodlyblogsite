8e7e352bc4ed55fcbab3452a06e2bafd0abfc162
Kevin Seo <moodlyme@gmail.com>
2013-01-17 17:17:08 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogsitemoodly::Application.config.secret_token = '9d374fe2eb3584da340bf1b191bd14ab6fd610a45541874df2485c3e0b8ed91d240ca3df3c601fb3ed6f918d520c84a85b26d9c0cb710f4fe45dac1f168eade1'
